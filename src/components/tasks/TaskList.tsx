3a2ac3ff3ca33fbee8e3f670c8f1d5f5bc1942b4
Raiki215 <r.fujiwara.sys22@morijyobi.ac.jp>
2025-09-12 15:00:40 +0900

    todoの一覧取得

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -2,9 +2,69 @@
 import TaskCard from "@/components/tasks/TaskCard";
 import { useTaskStore } from "@/lib/store";
 import { getWeekDates } from "@/utils/date";
+import { useEffect } from "react";
 
 export default function TaskList() {
-  const { selectedDate, tasks, highlightTaskId, viewMode } = useTaskStore();
+  const { selectedDate, tasks, highlightTaskId, viewMode, addTask } =
+    useTaskStore();
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000//get_user_todos", {
+          method: "GET",
+          credentials: "include",
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+
+        const formattedTasks = result.datas.map(
+          (task: {
+            todo_id: string;
+            todo: string;
+            deadline?: string;
+            priority: number;
+            duration?: number;
+            finish_flg: number;
+            estimated_time?: string | number;
+            tags?: string[];
+          }) => {
+            let time = "";
+            let date = "";
+            if (task.deadline) {
+              const d = new Date(task.deadline);
+              const hours = d.getUTCHours().toString().padStart(2, "0");
+              const minutes = d.getUTCMinutes().toString().padStart(2, "0");
+              time = `${hours}:${minutes}`;
+
+              const year = d.getUTCFullYear();
+              const month = (d.getUTCMonth() + 1).toString().padStart(2, "0");
+              const day = d.getUTCDate().toString().padStart(2, "0");
+              date = `${year}-${month}-${day}`;
+            }
+            addTask({
+              id: task.todo_id,
+              title: task.todo,
+              date: date,
+              priority: task.priority,
+              duration: Number(task.estimated_time),
+              tags: task.tags,
+              time: time,
+              status: task.finish_flg === 1 ? "完了" : "未完了",
+            });
+
+            console.log(task);
+          }
+        );
+      } catch (error) {
+        console.error("タスクの取得に失敗しました:", error);
+      }
+    };
+
+    fetchTasks();
+  }, [addTask]);
 
   // 表示するタスクを決定
   const items =
