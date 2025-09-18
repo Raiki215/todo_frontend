de3360ce71564b31966df5588a41342afdec1e99
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2025-09-18 12:32:05 +0900

    認証部分の追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -22,6 +22,9 @@ export default function LoginPage() {
 
   // 既ログインならホームへ
   useEffect(() => {
+    if (auth?.isLoading) {
+      return;
+    }
     if (auth?.isAuthenticated && auth?.user) {
       router.replace("/");
     }
